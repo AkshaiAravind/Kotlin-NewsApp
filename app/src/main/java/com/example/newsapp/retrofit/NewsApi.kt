7c0f582b2e324dfd6fde8f1650ea4ad491359ee7
AkshaiAravind <akshaiaravind614@gmail.com>
2024-11-20 19:48:51 +0530

    First commit

--- app/src/main/java/com/example/newsapp/retrofit/NewsApi.kt
+++ app/src/main/java/com/example/newsapp/retrofit/NewsApi.kt
@@ -1,19 +1,16 @@
-package com.example.newsapp.retrofit
-
-import retrofit2.Call
-import retrofit2.http.GET
-import retrofit2.http.Query
-
 interface NewsApi {
 
-    //https://newsapi.org/v2/top-headlines?country=in&category=business&apiKey=your_api_key
-
+    @GET("/v2/top-headlines")
+    fun getNews(
+        @Query("country") country: String,
+        @Query("category") category: String? = null, // Optional category
+        @Query("apiKey") key: String = "92e382ccdc0244e2b59e91cbb27431bc" // Default API key
+    ): Call<NewsDataFromJson>
 
     @GET("/v2/top-headlines")
-    fun getNews(@Query("country") country : String, @Query("category") category : String?, @Query("apiKey") key : String) : Call<NewsDataFromJson>
-
-
-    // for global
-//    https://newsapi.org/v2/top-headlines?category=general&language=en&apiKey=5a3e054de1834138a2fbc4a75ee69053
-
+    fun getGlobalNews(
+        @Query("category") category: String = "general", // Default category
+        @Query("language") language: String = "en", // Default language
+        @Query("apiKey") key: String = "92e382ccdc0244e2b59e91cbb27431bc" // Default API key
+    ): Call<NewsDataFromJson>
 }
